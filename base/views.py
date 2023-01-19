e809912551fb6a085f7b6afdeb6b6521fac55c1f
Praveen Jeyachandran <praveen3970g@gmail.com>
2023-01-19 15:10:53 +0530

    creating rooms

--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 1, 'name': 'Design with me !'},
+    {'id': 1, 'name': 'Frontend Devs!'},
+]
+
 
 def home(request):
     return render(request, 'home.html')
