6a83b443d5ec1b064d69cc75551f6b2484421779
Praveen Jeyachandran <praveen3970g@gmail.com>
2023-01-28 17:43:25 +0530

    room, topic, user and messages models created

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Room
 
 rooms = [
     {'id': 1, 'name': 'Lets learn python!'},
@@ -8,16 +9,13 @@ rooms = [
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            print(room)
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
